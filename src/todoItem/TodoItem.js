c30125198202f3af58e8002794f3afa0063aa593
Danni Douglas <danni.douglas@wpengine.com>
2022-11-17 15:02:48 -0600

    wip: added editmode and input form for updating, but not update capabilities

--- src/todoItem/TodoItem.js
+++ src/todoItem/TodoItem.js
@@ -1,14 +1,40 @@
+import React from 'react';
 import Card from '@mui/material/Card';
 import PropTypes from 'prop-types';
 
-
 function TodoItem(props) {
-    return <Card>
+    const [editMode, setEditMode] = React.useState(false);
+  
+    if (!editMode) {
+      return <Card>
+      <button onClick={() => {setEditMode(!editMode)}}>Edit</button>
+      <span>{editMode.toString()}</span>
       <span>{props.title}</span>
       <span>{props.completed}</span>
       <span>{props.description}</span>
       <span>{props.createdOn}</span>
     </Card>
+    } else {
+      return <Card>
+        <form>
+          <label>
+            Title:
+            <input type="text" name="title" />
+          </label>
+          <label>
+            Description:
+            <input type="text" name="description" />
+          </label>
+          <label>
+            Completed:
+            <input type="checkbox" name="completed" />
+          </label>
+          <button onClick={() => {setEditMode(!editMode)}}>Cancel</button>
+          <input onClick={() => {setEditMode(!editMode)}} type="submit" value="Submit" />
+        </form>
+      </Card>
+    }
+    
   }
 
   TodoItem.propTypes = {
